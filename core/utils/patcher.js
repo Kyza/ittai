b109888d3f314a4ab7e4c2f33968cb58c016b7df
Kyza <tylerc2077@gmail.com>
2021-01-09 06:28:40 -0500

    Better patcher structure and bugfixes

--- core/utils/patcher.js
+++ core/utils/patcher.js
@@ -1,99 +1,96 @@
 import logger from "./logger";
 
-function dirtyObject(object) {
-	if (!object.__ittaiPatches__) object.__ittaiPatches__ = [];
-}
-
-function cleanObject(object) {
-	if (object.__ittaiPatches__) delete object.__ittaiPatches__;
-}
-
 export default {
 	patch: function (name, object, functionName, type, patch) {
-		dirtyObject(object);
+		if (!object.__ittai__) object.__ittai__ = {};
 		const patchData = {
 			name,
 			type,
-			functionName,
 			patch,
-			original: { ...object }[functionName],
 			unpatch: function () {
 				try {
-					const patchIndex = object.__ittaiPatches__.indexOf(this);
+					const patchIndex = object.__ittai__[functionName].patches.indexOf(
+						this
+					);
 					if (patchIndex === -1)
 						throw "Couldn't find the patch. This probably happened because the object was tampered with. Don't do that.";
-					const currentPatch = object.__ittaiPatches__[patchIndex];
+					const currentPatch =
+						object.__ittai__[functionName].patches[patchIndex];
 					// Restore original function.
-					object[functionName] = currentPatch.original;
+					object[functionName] = object.__ittai__[functionName].original;
 					// Delete patch.
-					object.__ittaiPatches__.splice(patchIndex, 1);
+					object.__ittai__[functionName].patches.splice(patchIndex, 1);
 					// Clean up the object if there are no patches left.
-					if (object.__ittaiPatches__.length === 0) cleanObject(object);
+					if (!object.__ittai__[functionName].patches.length) {
+						delete object.__ittai__[functionName];
+					}
+					if (!Object.keys(object.__ittai__).length) {
+						delete object.__ittai__;
+					}
 				} catch (e) {
 					logger.error(`Failed to unpatch ${name}.`, e);
 				}
 			},
 		};
-		object.__ittaiPatches__.push(patchData);
 
-		if (object[functionName].__ittaiPatched__) return patchData;
+		if (!object.__ittai__[functionName]) {
+			object.__ittai__[functionName] = {
+				original: { ...object }[functionName],
+				patches: [],
+			};
 
-		const props = { ...object[functionName] };
+			const props = { ...object[functionName] };
 
-		object[functionName] = function (...args) {
-			const functionPatches = object.__ittaiPatches__.filter(
-				(p) => p.functionName === functionName
-			);
-			const befores = functionPatches.filter((p) => p.type === "before");
-			const insteads = functionPatches.filter((p) => p.type === "instead");
-			const afters = functionPatches.filter((p) => p.type === "after");
+			object[functionName] = function (...args) {
+				const functionData = object.__ittai__[functionName];
+				const befores = functionData.patches.filter((p) => p.type === "before");
+				const insteads = functionData.patches.filter(
+					(p) => p.type === "instead"
+				);
+				const afters = functionData.patches.filter((p) => p.type === "after");
 
-			// Before patches.
-			for (const before of befores) {
-				try {
-					args = before.patch(args);
-				} catch (e) {
-					logger.error(`Error running before patch ${name}.`, e);
-				}
-			}
-
-			// Instead patches.
-			let res = {};
-			if (insteads.length === 0) {
-				try {
-					res = patchData.original.call(this, ...args);
-				} catch (e) {
-					logger.error(`Error running instead patch ${name}.`, e);
-				}
-			} else {
-				// Bad, fix later.
-				for (const instead of insteads) {
-					// Do trash merge with Lodash.
+				// Before patches.
+				for (const before of befores) {
 					try {
-						res = globalThis._.merge(res, instead.patch(args));
+						args = before.patch(args);
 					} catch (e) {
-						logger.error(`Error running instead patch ${name}.`, e);
+						logger.error(`Error running before patch ${name}.`, e);
 					}
 				}
-			}
 
-			// After patches.
-			for (const after of afters) {
-				try {
-					res = after.patch(args, res);
-				} catch (e) {
-					logger.error(`Error running after patch ${name}.`, e);
+				// Instead patches.
+				let res = {};
+				if (insteads.length === 0) {
+					res = functionData.original.call(this, ...args);
+				} else {
+					// Bad, fix later.
+					for (const instead of insteads) {
+						// Do trash merge with Lodash.
+						try {
+							res = globalThis._.merge(res, instead.patch(args));
+						} catch (e) {
+							logger.error(`Error running instead patch ${name}.`, e);
+						}
+					}
 				}
+
+				// After patches.
+				for (const after of afters) {
+					try {
+						res = after.patch(args, res);
+					} catch (e) {
+						logger.error(`Error running after patch ${name}.`, e);
+					}
+				}
+
+				return res;
+			};
+
+			for (const key in props) {
+				object[functionName][key] = props[key];
 			}
-
-			return res;
-		};
-
-		for (const key in props) {
-			object[functionName][key] = props[key];
 		}
-
-		object[functionName].__ittaiPatched__ = true;
+		object.__ittai__[functionName].patches.push(patchData);
 
 		return patchData;
 	},
